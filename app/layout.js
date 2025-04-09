8fb6d20f4de6e8a196477688a2debefcbe9db50c
bookingsgenie <aymankalsi22@gmail.com>
2025-04-09 15:01:56 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,23 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Chatbase Widget Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.chatbaseConfig = {
+                chatbotId: "cltcqkqlg0004me2y5sz8b9qj"
+              };
+            `,
+          }}
+        />
+        <script
+          src="https://www.chatbase.co/embed.min.js"
+          id="chatbase-script"
+          defer
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
